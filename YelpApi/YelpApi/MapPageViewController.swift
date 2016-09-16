721379f322510e25a0253e3f9fe5fd6e468ee1df
kevintbradbury <kevin.t.bradbury@gmail.com>
2016-09-16 07:01:45 -0700

    Populate MapView Page
    can populate map, haven’t coded location delegate yet. Also need to
    determine how to populate yelp data and pin to locations.

---
+++ YelpApi/YelpApi/MapPageViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MapPageViewController.swift
+//  YelpApi
+//
+//  Created by K on 9/16/16.
+//  Copyright © 2016 K. All rights reserved.
+//
+
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapPageViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    var locationManager: CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
+        
+        
+        
+    }
+    
+}
---
+++ YelpApi/YelpApi/TreasureHuntViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TreasureHuntViewController.swift
+//  YelpApi
+//
+//  Created by K on 9/16/16.
+//  Copyright © 2016 K. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TeasureHuntViewController: UITableViewController {
+    
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
